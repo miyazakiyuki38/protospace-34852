91de108482128d7e809011b090b64e8dc0a732cc
miyazakiyuki38 <79497965+miyazakiyuki38@users.noreply.github.com>
2021-03-17 18:06:55 +0900

    first commitの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :index, :new]
-  before_action :move_to_index, except: :index
+  before_action :authenticate_user!, only: [:create, :destroy, :edit, :update]
+  # before_action :move_to_index, except: :index
 
   def index
     @prototypes = Prototype.all
